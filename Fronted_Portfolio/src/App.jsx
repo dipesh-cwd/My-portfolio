f65ac59952771c107c2aba2d37b9e929e1e72d63
dipesh-cwd <codewithdipesh44@gmail.com>
2025-11-22 15:03:50 +0545

    first commit

---
+++ Fronted_Portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hallo i am back!</h1>
+       <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
